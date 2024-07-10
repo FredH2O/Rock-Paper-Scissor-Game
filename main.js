7f42473665249fc78a5582e92c51bcff27bc5eaa
Fred <huertaserick@yahoo.ie>
2024-07-10 19:18:54 +0100

    changes

---
+++ main.js
@@ -0,0 +1,9 @@
+// rock paper scissor game
+
+// let choice = document.querySelectorAll(".choice");
+// let resultElement = document.getElementById("result");
+let opponent = ["rock", "paper", "scissors"];
+let opponentChoiceIndex = Math.floor(Math.random() * opponent.length);
+let opponentChoice = opponent[opponentChoiceIndex];
+
+function pickedChoice() {}
