4d6267c0fe01f4812905c76f9df81a87a95405c2
Fred <huertaserick@yahoo.ie>
2024-07-15 17:30:24 +0100

    Fixes

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ function disableButton() {
     button.disabled = true;
     setTimeout(function () {
       button.disabled = false;
-    }, 3000);
+    }, 6600);
   });
 }
 
@@ -62,127 +62,127 @@ function playGame(playerChoice) {
   document.getElementById("opponentGif").alt =
     opponentChoice.charAt(0).toUpperCase() + opponentChoice.slice(1);
 
-  if (playerChoice === opponentChoice) {
-    resultElement.textContent = "It's a Draw! 😐";
-  } else if (
-    (playerChoice === "rock" && opponentChoice === "scissors") ||
-    (playerChoice === "paper" && opponentChoice === "rock") ||
-    (playerChoice === "scissors" && opponentChoice === "paper")
-  ) {
-    resultElement.textContent = "You Win! 😄";
-    playerScore++;
-  } else {
-    resultElement.textContent = "You Lose! 💀";
-    opponentScore++;
-  }
+  setTimeout(function () {
+    if (playerChoice === opponentChoice) {
+      resultElement.textContent = "It's a Draw! 😐";
+    } else if (
+      (playerChoice === "rock" && opponentChoice === "scissors") ||
+      (playerChoice === "paper" && opponentChoice === "rock") ||
+      (playerChoice === "scissors" && opponentChoice === "paper")
+    ) {
+      resultElement.textContent = "You Win! 😄";
+      playerScore++;
+    } else {
+      resultElement.textContent = "You Lose! 💀";
+      opponentScore++;
+    }
+    playerScoreElement.textContent = playerScore;
+    opponentScoreElement.textContent = opponentScore;
 
-  playerScoreElement.textContent = playerScore;
-  opponentScoreElement.textContent = opponentScore;
+    if (playerScore >= 3) {
+      resultElement.textContent = `Congratulations you won!!`;
+      playerScore = 0;
+      opponentScore = 0;
+      playerScoreElement.textContent = 0;
+      opponentScoreElement.textContent = 0;
+      soundEffects.src = "sounds/winnerSound.mp3";
+      soundEffects.volume = 1;
+      soundEffects.play();
 
-  if (playerScore >= 3) {
-    resultElement.textContent = `Congratulations you won!!`;
-    playerScore = 0;
-    opponentScore = 0;
-    playerScoreElement.textContent = 0;
-    opponentScoreElement.textContent = 0;
-    soundEffects.src = "sounds/winnerSound.mp3";
-    soundEffects.volume = 1;
-    soundEffects.play();
+      // confetti.js library down here
+      const count = 300,
+        defaults = {
+          origin: { y: 0.5 },
+        };
 
-    // confetti.js library down here
-    const count = 300,
-      defaults = {
-        origin: { y: 0.5 },
+      function fire(particleRatio, opts) {
+        confetti(
+          Object.assign({}, defaults, opts, {
+            particleCount: Math.floor(count * particleRatio),
+          })
+        );
+      }
+
+      fire(0.25, {
+        spread: 26,
+        startVelocity: 55,
+      });
+
+      fire(0.2, {
+        spread: 60,
+      });
+
+      fire(0.35, {
+        spread: 100,
+        decay: 0.91,
+        scalar: 0.8,
+      });
+
+      fire(0.1, {
+        spread: 120,
+        startVelocity: 25,
+        decay: 0.92,
+        scalar: 1.2,
+      });
+
+      fire(0.1, {
+        spread: 120,
+        startVelocity: 45,
+      });
+
+      // confetti.js library up here
+    }
+    if (opponentScore >= 3) {
+      resultElement.textContent = `You LOSE!`;
+      soundEffects.src = "sounds/loseSound.mp3";
+      soundEffects.volume = 1;
+      soundEffects.play();
+      playerScore = 0;
+      opponentScore = 0;
+      playerScoreElement.textContent = 0;
+      opponentScoreElement.textContent = 0;
+      // confetti.js here
+      const defaults = {
+        spread: 760,
+        ticks: 100,
+        gravity: 0,
+        decay: 0.94,
+        startVelocity: 30,
       };
 
-    function fire(particleRatio, opts) {
-      confetti(
-        Object.assign({}, defaults, opts, {
-          particleCount: Math.floor(count * particleRatio),
-        })
-      );
-    }
+      function shoot() {
+        confetti({
+          ...defaults,
+          particleCount: 30,
+          scalar: 1.2,
+          shapes: ["circle", "square"],
+          colors: ["#0000", "#0000", "#0000", "#0000", "#0000"],
+        });
 
-    fire(0.25, {
-      spread: 26,
-      startVelocity: 55,
-    });
-
-    fire(0.2, {
-      spread: 60,
-    });
-
-    fire(0.35, {
-      spread: 100,
-      decay: 0.91,
-      scalar: 0.8,
-    });
-
-    fire(0.1, {
-      spread: 120,
-      startVelocity: 25,
-      decay: 0.92,
-      scalar: 1.2,
-    });
-
-    fire(0.1, {
-      spread: 120,
-      startVelocity: 45,
-    });
-
-    // confetti.js library up here
-  }
-
-  if (opponentScore >= 3) {
-    resultElement.textContent = `You LOSE!`;
-    soundEffects.src = "sounds/loseSound.mp3";
-    soundEffects.volume = 1;
-    soundEffects.play();
-    playerScore = 0;
-    opponentScore = 0;
-    playerScoreElement.textContent = 0;
-    opponentScoreElement.textContent = 0;
-    // confetti.js here
-    const defaults = {
-      spread: 760,
-      ticks: 100,
-      gravity: 0,
-      decay: 0.94,
-      startVelocity: 30,
-    };
-
-    function shoot() {
-      confetti({
-        ...defaults,
-        particleCount: 30,
-        scalar: 1.2,
-        shapes: ["circle", "square"],
-        colors: ["#0000", "#0000", "#0000", "#0000", "#0000"],
-      });
-
-      confetti({
-        ...defaults,
-        particleCount: 100,
-        scalar: 2,
-        shapes: ["emoji"],
-        shapeOptions: {
-          emoji: {
-            value: ["💩", "💀"],
+        confetti({
+          ...defaults,
+          particleCount: 100,
+          scalar: 2,
+          shapes: ["emoji"],
+          shapeOptions: {
+            emoji: {
+              value: ["💩", "💀"],
+            },
           },
-        },
-      });
+        });
+      }
+
+      setTimeout(shoot, 0);
+      setTimeout(shoot, 100);
+      setTimeout(shoot, 200);
+
+      // confetti.js here
     }
-
-    setTimeout(shoot, 0);
-    setTimeout(shoot, 100);
-    setTimeout(shoot, 200);
-
-    // confetti.js here
-  }
+  }, 1500);
 }
 
 function startCountdown() {
-  let x = 3;
+  let x = 4;
   timerElement.textContent = x;
 
   if (countdownTimer) {
@@ -199,7 +199,7 @@ function startCountdown() {
       document.getElementById("opponentGif").src = "";
       document.getElementById("opponentGif").alt = "";
       resultElement.textContent = "Make a choice!";
-      timerElement.textContent = 3;
+      timerElement.textContent = x;
       clearInterval(countdownTimer);
     }
   }, 1000);
